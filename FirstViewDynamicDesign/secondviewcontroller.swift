c39cffbd7d0910fe80e1d219f25db9168257ecca
asaithambiduraimurugan <asaiios435@gmail.com>
2024-03-27 15:18:05 +0530

    level init

---
+++ FirstViewDynamicDesign/secondviewcontroller.swift
@@ -0,0 +1,20 @@
+//
+//  secondviewcontroller.swift
+//  FirstViewDynamicDesign
+//
+//  Created by asaithambi duraimurugan on 27/03/24.
+//
+
+import UIKit
+
+class secondviewcontroller: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
